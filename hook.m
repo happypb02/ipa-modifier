4a83feec035e699f1e81c4ce3e354422daaf3f54
Your Name <your-email@example.com>
2026-05-04 07:09:26 +0800

    Back to dylib injection with simplified hook

--- hook.m
+++ hook.m
@@ -2,60 +2,46 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 
-@interface DASignProcessVC : UIViewController
-@end
-
-@interface DASelectAppVC : UIViewController
-- (void)signSuccess;
-@end
-
+// Minimal hook that swizzles installClick
 __attribute__((constructor))
 static void hook_init() {
-    NSLog(@"[Hook] Initializing hook...");
+    @autoreleasepool {
+        NSLog(@"[Hook] Loading...");
 
-    Class cls = objc_getClass("DASignProcessVC");
-    if (!cls) {
-        NSLog(@"[Hook] DASignProcessVC class not found");
-        return;
-    }
-    NSLog(@"[Hook] Found DASignProcessVC class");
-
-    Method origMethod = class_getInstanceMethod(cls, @selector(installClick));
-    if (!origMethod) {
-        NSLog(@"[Hook] installClick method not found");
-        return;
-    }
-    NSLog(@"[Hook] Found installClick method");
-
-    IMP origIMP = method_getImplementation(origMethod);
-
-    IMP newIMP = imp_implementationWithBlock(^(id self) {
-        NSLog(@"[Hook] installClick intercepted");
-
-        // Get selectVC using KVC
-        id selectVC = nil;
-        @try {
-            selectVC = [self valueForKey:@"selectVC"];
-            NSLog(@"[Hook] selectVC = %@", selectVC);
-        } @catch (NSException *e) {
-            NSLog(@"[Hook] Failed to get selectVC: %@", e);
-        }
-
-        if (!selectVC) {
-            NSLog(@"[Hook] selectVC is nil, calling original");
-            ((void(*)(id,SEL))origIMP)(self, @selector(installClick));
+        Class cls = objc_getClass("DASignProcessVC");
+        if (!cls) {
+            NSLog(@"[Hook] DASignProcessVC not found");
             return;
         }
 
-        // Call signSuccess
-        if ([selectVC respondsToSelector:@selector(signSuccess)]) {
-            [selectVC performSelector:@selector(signSuccess)];
-            NSLog(@"[Hook] Called signSuccess");
-        } else {
-            NSLog(@"[Hook] signSuccess not found");
+        SEL origSel = @selector(installClick);
+        Method origMethod = class_getInstanceMethod(cls, origSel);
+        if (!origMethod) {
+            NSLog(@"[Hook] installClick method not found");
+            return;
         }
-    });
 
-    method_setImplementation(origMethod, newIMP);
-    NSLog(@"[Hook] Successfully hooked installClick");
+        // Replace with a block that does the redirect
+        IMP newIMP = imp_implementationWithBlock(^(UIViewController *self) {
+            NSLog(@"[Hook] installClick called");
+
+            // Try to get selectVC via KVC
+            id selectVC = nil;
+            @try {
+                selectVC = [self valueForKey:@"selectVC"];
+            } @catch (NSException *e) {
+                NSLog(@"[Hook] Failed to get selectVC: %@", e);
+            }
+
+            if (selectVC && [selectVC respondsToSelector:@selector(signSuccess)]) {
+                NSLog(@"[Hook] Calling signSuccess");
+                [selectVC performSelector:@selector(signSuccess)];
+            } else {
+                NSLog(@"[Hook] selectVC not available or signSuccess not found");
+            }
+        });
+
+        method_setImplementation(origMethod, newIMP);
+        NSLog(@"[Hook] Successfully hooked installClick");
+    }
 }
